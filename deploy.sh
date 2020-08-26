06b6685e78aa0891131476dde6c3b2b831e82680
Christopher Hiller <boneskull@boneskull.com>
2020-08-26 15:59:49 -0700

    add some notes to deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# This script clones the `mochajs.org` branch of
+# https://github.com/mochajs/mocha--which is the branch that the site at
+# https://mochajs.org is built from--into a `mocha` subdirectory.  It then
+# installs the current version of `@mocha/docdash` over whatever is currently
+# used in the `mochajs.org` branch, and builds Mocha's documentation using it.
+# This way, we can see the proposed changes to this theme as they would affect
+# the site. 
+
 set -o errexit
 set -o nounset
 set -o pipefail
